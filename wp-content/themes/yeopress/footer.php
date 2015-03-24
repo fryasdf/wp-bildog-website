bb5cee8f98b859d0f570b5db2787369b9c9aba9d
Fabian Werner <fab_wer@web.de>
2015-03-24 13:06:28 +0100

    updated plugins, changed layout of footer

--- wp-content/themes/yeopress/footer.php
+++ wp-content/themes/yeopress/footer.php
@@ -1,36 +1,51 @@
 <footer id="page-footer" class="main-column">
- <div class="row">
-    <div class="col-md-11">
-      <div class="row">
-        <div class="col-md-8">
-          bildog wird unterstützt von:<br/><br/>
-          <?php foreach (range(1, 20) as $number):
-            if(!get_custom('supporter_' . $number)) {continue;} ?>
-              <img src="<?php echo get_custom('supporter_' . $number); ?>" />
-          <?php endforeach; ?>
-        </div>
-        <div class="col-md-4">
-          <a href="<?php echo getPageLinkByTitle('Kontakt') ?>">Kontakt</a><br/>
-          <a href="<?php echo getPageLinkByTitle('Impressum') ?>">Impressum</a><br/>
-          <a href="<?php echo getPageLinkByTitle('Haftungsausschluss') ?>">Haftungsausschluss</a><br/>
-          <a href="<?php echo getPageLinkByTitle('Datenschutzerklärung') ?>">Datenschutzerklärung</a><br/>
-        </div>
-      </div>
+  <div class="row">
+    <?php // BILDOG WIRD UNTERSTUETZT SPALTE: 8?>
+    <div class="col-md-8 smaller-than-md-center">
+      bildog wird unterstützt von:<br/><br/>
+      <?php foreach (range(1, 20) as $number):
+        if(!get_custom('supporter_' . $number)) {continue;} ?>
+          <img src="<?php echo get_custom('supporter_' . $number); ?>" />
+      <?php endforeach; ?>
     </div>
-    <div class="col-md-1">
+    <?php // KONTAKT-UND-SOCIAL MEDIA SPALTE: 4?>
+    <div class="col-md-4">
       <div class="row">
-        <div class="col-md-12 icon-fb">
-          <a href="https://de-de.facebook.com/bildung.ohnegrenzen">
-            <img src="<?php echo bloginfo('template_directory') ?>/images/icon_fb.png" />
-          </a>
+        <?php // KONTAKT SPALTE : 10?>
+        <div class="col-xs-10">
+          <a href="<?php 
+             echo getPageLinkByTitle('Kontakt') 
+             ?>">Kontakt</a><br/>
+          <a href="<?php 
+             echo getPageLinkByTitle('Impressum') 
+             ?>">Impressum</a><br/>
+          <a href="<?php 
+             echo getPageLinkByTitle('Haftungsausschluss') 
+             ?>">Haftungsausschluss</a><br/>
+          <a href="<?php 
+             echo getPageLinkByTitle('Datenschutzerklärung') 
+             ?>">Datenschutzerklärung</a><br/>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12 icon-yt">
-          <img src="<?php echo bloginfo('template_directory') ?>/images/icon_yt.png" />
+        <?php // SOCIAL MEDIA SPALTE : 2?>
+        <div class="col-xs-2">
+          <div class="row">
+            <div class="col-md-12 icon-fb">
+              <a href="https://de-de.facebook.com/bildung.ohnegrenzen">
+                <img src="<?php echo bloginfo('template_directory') ?>/images/icon_fb.png" />
+              </a>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 icon-yt">
+              <img src="<?php echo bloginfo('template_directory') ?>/images/icon_yt.png" />
+            </div>
+          </div>
         </div>
+      <?php // ENDE SOCIAL MEDIA SPALTE ?>
       </div>
+    <?php // ENDE ROW ?>
     </div>
+  <?php // ENDE KONTAKT-UND-SOCIAL MEDIA SPALTE?>
   </div>
   <div class="row copyright">
     copyright 2014 bildog Bildung ohne Grenzen e.V.
