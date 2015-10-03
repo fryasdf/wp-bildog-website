b15025f8985c69381af002dbb90105a237b4e2ce
Fabian Werner <fab_wer@web.de>
2015-10-03 19:24:16 +0200

    updated to wordpress 4.3.1

--- wp-content/plugins/slideshow-jquery-image-gallery/views/SlideshowPluginShortcode/shortcode-inserter.php
+++ wp-content/plugins/slideshow-jquery-image-gallery/views/SlideshowPluginShortcode/shortcode-inserter.php
@@ -1,82 +1,79 @@
-<a
-	href="#TB_inline?width=450&amp;inlineId=insertSlideshowShortcode"
-	class="button thickbox"
-	title="<?php _e('Insert a Slideshow', 'slideshow-plugin'); ?>"
-    style="padding-left: .4em;"
->
-	<?php global $wp_version; ?>
-	<?php if (version_compare($wp_version, '3.8', '>')) : ?>
-	<span class="dashicons dashicons-format-gallery" style="line-height: 26px;"></span>
-	<?php else: ?>
-	<img
-		src="<?php echo SlideshowPluginMain::getPluginUrl() . '/images/SlideshowPluginPostType/adminIcon.png'; ?>"
-		alt="<?php _e('Insert a Slideshow', 'slideshow-plugin'); ?>"
-	    style="vertical-align: text-top;"
-	/>
-	<?php endif; ?>
+<?php if ($data instanceof stdClass) : ?>
 
-	<?php _e('Insert Slideshow', 'slideshow-plugin'); ?>
-</a>
+	<a
+		href="#TB_inline?width=450&inlineId=insertSlideshowShortcode"
+		class="button thickbox"
+		title="<?php _e('Insert a Slideshow', 'slideshow-jquery-image-gallery'); ?>"
+	    style="padding-left: .4em;"
+	>
+		<img
+			src="<?php echo SlideshowPluginMain::getPluginUrl() . '/images/SlideshowPluginPostType/adminIcon.png'; ?>"
+			alt="<?php _e('Insert a Slideshow', 'slideshow-jquery-image-gallery'); ?>"
+		    style="vertical-align: text-top;"
+		/>
+		<?php _e('Insert Slideshow', 'slideshow-jquery-image-gallery'); ?>
+	</a>
 
-<div id="insertSlideshowShortcode" style="display: none;">
+	<div id="insertSlideshowShortcode" style="display: none;">
 
-	<h3 style="padding: 10px 0; color: #5a5a5a;">
-		<?php _e('Insert a Slideshow', 'slideshow-plugin'); ?>
-	</h3>
+		<h3 style="padding: 10px 0; color: #5a5a5a;">
+			<?php _e('Insert a Slideshow', 'slideshow-jquery-image-gallery'); ?>
+		</h3>
 
-	<div style="border: 1px solid #ddd; padding: 10px; color: #5a5a5a;">
+		<div style="border: 1px solid #ddd; padding: 10px; color: #5a5a5a;">
 
-		<?php if($slideshows instanceof WP_Query && count($slideshows->get_posts()) > 0): ?>
-		<table>
-			<tr>
+			<?php if($data->slideshows instanceof WP_Query && count($data->slideshows->get_posts()) > 0): ?>
+			<table>
+				<tr>
 
-				<td><?php _e('Select a slideshow', 'slideshow-plugin'); ?></td>
-				<td>
-					<select id="insertSlideshowShortcodeSlideshowSelect">
+					<td><?php _e('Select a slideshow', 'slideshow-jquery-image-gallery'); ?></td>
+					<td>
+						<select id="insertSlideshowShortcodeSlideshowSelect">
 
-						<?php foreach($slideshows->get_posts() as $slideshow): ?>
+							<?php foreach($data->slideshows->get_posts() as $slideshow): ?>
 
-						<?php if(!is_numeric($slideshow->ID)) continue; ?>
+							<?php if(!is_numeric($slideshow->ID)) continue; ?>
 
-						<option value="<?php echo $slideshow->ID; ?>">
-							<?php echo (!empty($slideshow->post_title)) ? htmlspecialchars($slideshow->post_title) : __('Untitled slideshow', 'slideshow-plugin'); ?>
-						</option>
+							<option value="<?php echo $slideshow->ID; ?>">
+								<?php echo (!empty($slideshow->post_title)) ? htmlspecialchars($slideshow->post_title) : __('Untitled slideshow', 'slideshow-jquery-image-gallery'); ?>
+							</option>
 
-						<?php endforeach; ?>
+							<?php endforeach; ?>
 
-					</select>
-				</td>
+						</select>
+					</td>
 
-			</tr>
-			<tr>
+				</tr>
+				<tr>
 
-				<td>
-					<input
-						type="button"
-						class="button-primary insertSlideshowShortcodeSlideshowInsertButton"
-						value="<?php _e('Insert Slideshow', 'slideshow-plugin'); ?>"
-					/>
-					<input
-						type="button"
-						class="button insertSlideshowShortcodeCancelButton"
-					    value="<?php _e('Cancel', 'slideshow-plugin'); ?>"
-					/>
-				</td>
+					<td>
+						<input
+							type="button"
+							class="button-primary insertSlideshowShortcodeSlideshowInsertButton"
+							value="<?php _e('Insert Slideshow', 'slideshow-jquery-image-gallery'); ?>"
+						/>
+						<input
+							type="button"
+							class="button insertSlideshowShortcodeCancelButton"
+						    value="<?php _e('Cancel', 'slideshow-jquery-image-gallery'); ?>"
+						/>
+					</td>
 
-			</tr>
-		</table>
+				</tr>
+			</table>
 
-		<?php else: ?>
+			<?php else: ?>
 
-		<p>
-			<?php echo sprintf(
-				__('It seems you haven\'t created any slideshows yet. %sYou can create a slideshow here!%s', 'slideshow-plugin'),
-				'<a href="' . admin_url('post-new.php?post_type=' . SlideshowPluginPostType::$postType) . '" target="_blank">',
-				'</a>'
-			); ?>
-		</p>
+			<p>
+				<?php echo sprintf(
+					__('It seems you haven\'t created any slideshows yet. %sYou can create a slideshow here!%s', 'slideshow-jquery-image-gallery'),
+					'<a href="' . admin_url('post-new.php?post_type=' . SlideshowPluginPostType::$postType) . '" target="_blank">',
+					'</a>'
+				); ?>
+			</p>
 
-		<?php endif; ?>
+			<?php endif; ?>
 
-    </div>
-</div>
+	    </div>
+	</div>
+<?php endif; ?>
